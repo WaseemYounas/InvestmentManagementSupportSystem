c2f26a3799ef4e0b2b36e597dea66f271e2b13d6
WaseemYounas <wgcu418@gmail.com>
2019-10-28 23:15:12 +0500

    Home

--- InvestmentManagementSystem/Migrations/201910281626396_initial.Designer.cs
+++ InvestmentManagementSystem/Migrations/201910281626396_initial.Designer.cs
@@ -7,13 +7,13 @@ namespace InvestmentManagementSystem.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class abc : IMigrationMetadata
+    public sealed partial class initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(abc));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201910280949199_abc"; }
+            get { return "201910281626396_initial"; }
         }
         
         string IMigrationMetadata.Source
--- InvestmentManagementSystem/Migrations/201910281626396_initial.cs
+++ InvestmentManagementSystem/Migrations/201910281626396_initial.cs
@@ -3,19 +3,17 @@ namespace InvestmentManagementSystem.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class appointment : DbMigration
+    public partial class initial : DbMigration
     {
         public override void Up()
         {
             AddColumn("dbo.Appointments", "address", c => c.String());
             AddColumn("dbo.Appointments", "city", c => c.String());
             AddColumn("dbo.Appointments", "country", c => c.String());
-            DropColumn("dbo.Appointments", "location");
         }
         
         public override void Down()
         {
-            AddColumn("dbo.Appointments", "location", c => c.String());
             DropColumn("dbo.Appointments", "country");
             DropColumn("dbo.Appointments", "city");
             DropColumn("dbo.Appointments", "address");
